e2326f6d0629115be056898f6de01d81f0919fca
unknown <mobinaghashahi13@gmail.com>
2023-11-28 17:12:12 +0330

    create initial dataBase

--- database/migrations/2023_11_28_133217_news.php
+++ database/migrations/2023_11_28_133217_news.php
@@ -11,11 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('news', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('userName')->unique();
-            $table->string('password');
+            $table->string('text');
+            $table->string('title');
+            $table->string('instrument');
+            $table->boolean('important');
+            $table->integer('effect');
+            $table->string('comment');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -26,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('news');
     }
 };
