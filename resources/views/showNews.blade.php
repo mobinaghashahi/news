dbb7cc5ec3b5f55374859cdbd32403cbbba8c544
unknown <mobinaghashahi13@gmail.com>
2023-11-30 02:29:39 +0330

    create and design index page

---
+++ resources/views/showNews.blade.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+
+    <title>news</title>
+    <link rel="shortcut icon" href="/logo/logo.ico">
+
+    <meta name="title" content="NEWS">
+    <meta name="description"
+          content="Foreign Exchange Market News">
+    <meta name="keywords" content="News">
+    <meta name="robots" content="index, follow">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="language" content="en">
+    <meta name="author" content="Payam Paydar">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/css/style.css?v=4.7" rel="stylesheet">
+    <link href="/css/RWD.css" rel="stylesheet">
+    <header>
+        <div class="col-12" style="background-color: #4b5563;float: right">
+            <a class="col-12" style="color: white;text-decoration: none; text-align: center;margin: 15px 0px 15px 0px"
+               href="#">
+                NEWS
+            </a>
+        </div>
+    </header>
+</head>
+
+<body style="background-color: #1a202c">
+
+<div class="col-12" style="justify-content: center;display: flex">
+    <div class="col-6 blockNews">
+        <div class="col-12">
+            <p>This is a test message for all of the people in the world</p>
+        </div>
+        <div class="bottomBlockNews">
+            <div class="col-12">
+                <a>TITLE:</a>
+            </div>
+            <div class="col-12">
+                <a>DATE:</a>
+            </div>
+            <div class="col-12">
+                <a>INSTRUMENT:</a>
+            </div>
+            <div class="col-12">
+                <a>EFFECT:</a>
+            </div>
+            <div class="col-12">
+                <a>IMPORTANT:</a>
+            </div>
+            <div class="col-12">
+                <a>COMMENT:</a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="col-12" style="justify-content: center;display: flex">
+    <div class="col-6 blockNews">
+        <div class="col-12">
+            <p>This is a test message for all of the people in the world</p>
+        </div>
+        <div class="bottomBlockNews">
+            <div class="col-12">
+                <a>TITLE:</a>
+            </div>
+            <div class="col-12">
+                <a>DATE:</a>
+            </div>
+            <div class="col-12">
+                <a>INSTRUMENT:</a>
+            </div>
+            <div class="col-12">
+                <a>EFFECT:</a>
+            </div>
+            <div class="col-12">
+                <a>IMPORTANT:</a>
+            </div>
+            <div class="col-12">
+                <a>COMMENT:</a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="col-12" style="justify-content: center;display: flex">
+    <div class="col-6 blockNews">
+        <div class="col-12">
+            <p>This is a test message for all of the people in the world</p>
+        </div>
+        <div class="bottomBlockNews">
+            <div class="col-12">
+                <a>TITLE:</a>
+            </div>
+            <div class="col-12">
+                <a>DATE:</a>
+            </div>
+            <div class="col-12">
+                <a>INSTRUMENT:</a>
+            </div>
+            <div class="col-12">
+                <a>EFFECT:</a>
+            </div>
+            <div class="col-12">
+                <a>IMPORTANT:</a>
+            </div>
+            <div class="col-12">
+                <a>COMMENT:</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+
+
