152fb0829e10540739568006a04ac3859cde2f9f
unknown <mobinaghashahi13@gmail.com>
2023-11-30 17:51:29 +0330

    change structure design showNews page

--- resources/views/showNews.blade.php
+++ resources/views/showNews.blade.php
@@ -1,32 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head lang="en">
-
-    <title>news</title>
-    <link rel="shortcut icon" href="/logo/logo.ico">
-
-    <meta name="title" content="NEWS">
-    <meta name="description"
-          content="Foreign Exchange Market News">
-    <meta name="keywords" content="News">
-    <meta name="robots" content="index, follow">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="language" content="en">
-    <meta name="author" content="Payam Paydar">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/css/style.css?v=4.7" rel="stylesheet">
-    <link href="/css/RWD.css" rel="stylesheet">
-    <header>
-        <div class="col-12" style="background-color: #4b5563;float: right">
-            <a class="col-12" style="color: white;text-decoration: none; text-align: center;margin: 15px 0px 15px 0px"
-               href="#">
-                NEWS
-            </a>
-        </div>
-    </header>
-</head>
-
+@extends('layout.master')
+@section('content')
 <body style="background-color: #1a202c">
 
 <div class="col-12" style="justify-content: center;display: flex">
@@ -112,5 +85,6 @@
 </div>
 
 </body>
+@endsection
 
 
