88c67b8c58b5541159c787533275eb4dc798a89d
unknown <mobinaghashahi13@gmail.com>
2023-11-30 17:48:31 +0330

    create login view, routes and style

---
+++ resources/views/login/login.blade.php
@@ -0,0 +1,43 @@
+@extends('layout.master')
+
+@section('content')
+    <div class="col-12" style="display: flex;justify-content: center">
+        <div class="col-3 loginForm" style="padding: 50px 0px 50px 0px">
+            @if (\Session::has('msg'))
+                <div class="notification notificationSuccess">
+                    <p>{!! \Session::get('msg') !!}</p>
+                    <span class="notification_progress"></span>
+                </div>
+            @endif
+            @if ($errors->any())
+                @foreach($errors->all() as $error)
+                    <div class="notification notificationError">
+                        <p>{{$error}}</p>
+                        <span class="notification_progress"></span>
+                    </div>
+                @endforeach
+            @endif
+            <form method="post" name="enter" action="/login">
+                @csrf
+                <div class="col-12 divLabelInput">
+                    <a>نام کاربری</a>
+                </div>
+                <div class="col-12 divInputText">
+                    <input name="userName" type="text" class="inputText">
+                </div>
+
+                <div class="col-12 divLabelInput">
+                    <a>رمز عبور</a>
+                </div>
+                <div class="col-12 divInputText">
+                    <input name="password" type="password" class="inputText">
+                </div>
+
+                <div class="col-12 divInputSubmit">
+                    <input name="enter" class="inputSubmit" type="submit" value="ورود">
+                </div>
+
+            </form>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\authMiddleware;
+use App\Http\Controllers\Auth\loginController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('showNews');
 });
+Route::get('/login', [loginController::class, 'loginView'])->name('login')->middleware(authMiddleware::class);; //روت نمایش ویو صفحه ورود
+Route::post('/login', [loginController::class, 'login']); //روت پست اطلاعات فرم ورود
+Route::get('/logout', [loginController::class, 'logout']);
