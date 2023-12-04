d2ee9ef894080cf5d8b2967d743be663eb87705a
unknown <mobinaghashahi13@gmail.com>
2023-12-04 16:09:58 +0330

    reverse quary for show news

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function showHomePage(){
-        return view('showNews', ['news' => News::all()]);
+        return view('showNews', ['news' => News::all()->reverse()]);
     }
 }
