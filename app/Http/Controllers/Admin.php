82c9331004455c3009029b953a7e285b9f3b3090
unknown <mobinaghashahi13@gmail.com>
2023-12-06 09:24:19 +0330

    create function for edit news in controller admin

--- app/Http/Controllers/Admin.php
+++ app/Http/Controllers/Admin.php
@@ -35,16 +35,52 @@ class Admin extends Controller
         $news->text = $request->text;
         $news->user_id = Auth::user()->id;
         //اگر تیک important زده شده بود مقدار یک را ذخیره میکنیم و اگر زده نشده بود مقدار 0
-        if(isset($request->important))
+        if (isset($request->important))
             $news->important = 1;
         else
             $news->important = 0;
         $news->save();
 
         //ذخیره کردن هشتگ ها
-        saveTags($request->instrument,$news->id);
+        saveTags($request->instrument, $news->id);
 
         return redirect()->intended('/admin/addNews')->with('msg', 'خبر با موفقیت افزوده شد.');
 
+    }
+
+    public function onlineEdit()
+    {
+        glueTags(8);
+        return view('admin.onlineEdit', ['news' => News::all()->reverse(),
+            'tags' => retriveTags()]);
+    }
+
+    public function editNews(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required',
+            'effect' => 'required|integer',
+            'text' => 'required'
+        ]);
+
+        $news = News::findOrFail($request->id);
+        $news->title = $request->title;
+        $news->effect = $request->effect;
+        $news->comment = $request->comment;
+        $news->text = $request->text;
+        $news->user_id = Auth::user()->id;
+        //اگر تیک important زده شده بود مقدار یک را ذخیره میکنیم و اگر زده نشده بود مقدار 0
+        if (isset($request->important))
+            $news->important = 1;
+        else
+            $news->important = 0;
+        $news->save();
+
+        //حذف کردن تگ های قبلی
+        deleteTags($request->id);
+        //ذخیره کردن هشتگ ها
+        saveTags($request->instrument, $news->id);
+
+        return redirect()->intended('/admin/onlineEdit')->with('msg', 'خبر با موفقیت افزوده شد.');
     }
 }
