23ebe94c0175eeddb2e80960269034aec71260e8
unknown <mobinaghashahi13@gmail.com>
2023-12-06 18:05:40 +0330

    create userLogin Middleware

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,15 @@ use App\Http\Middleware\authMiddleware;
 use App\Http\Controllers\Auth\loginController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Admin;
+use App\Http\Middleware\loginMiddleware;
 
 
 Route::get('/',[HomeController::class, 'showHomePage']);
-Route::get('/login', [loginController::class, 'loginView'])->name('login')->middleware(authMiddleware::class);; //روت نمایش ویو صفحه ورود
+Route::get('/login', [loginController::class, 'loginView'])->name('login')->middleware(authMiddleware::class); //روت نمایش ویو صفحه ورود
 Route::post('/login', [loginController::class, 'login']); //روت پست اطلاعات فرم ورود
 Route::get('/logout', [loginController::class, 'logout']);
 
-Route::prefix('/admin')->group(function () {
+Route::prefix('/admin')->middleware([loginMiddleware::class])->group(function () {
     Route::get('/', [admin::class, 'showDashboard']);
 
     Route::get('/addNews', [admin::class, 'showAddNewsForm']);
