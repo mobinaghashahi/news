99bb42b91df24858751fdffa073de8e08c769169
unknown <mobinaghashahi13@gmail.com>
2023-12-23 16:29:26 +0330

    change order columns in export excel

--- app/Exports/NewsExport.php
+++ app/Exports/NewsExport.php
@@ -19,7 +19,7 @@ class NewsExport implements FromCollection, WithHeadings, ShouldAutoSize, WithEv
     public function headings(): array
     {
         return [
-            "TEXT", "TITLE", "IMPORTANT","EFFECT","COMMENT","INSTRUMENT"
+             "TITLE","DATE", "INSTRUMENT", "EFFECT", "COMMENT","NEWS",
         ];
     }
 
@@ -29,41 +29,38 @@ class NewsExport implements FromCollection, WithHeadings, ShouldAutoSize, WithEv
      */
     public function collection()
     {
-        $col2=new Collection([]);
-        $news=News::all()->reverse();
-        $tags=retriveTags();
-        $details=Details::all();
-        $added=false;
-        $index=0;
-        foreach ($news as $new)
-        {
-            foreach($details as $detail)
-            {
-                $col1=new Collection([
-                    [$new->text, $new->title,"","","",""]
+        $col2 = new Collection([]);
+        $news = News::all()->reverse();
+        $tags = retriveTags();
+        $details = Details::all();
+        $added = false;
+        $index = 0;
+        foreach ($news as $new) {
+            foreach ($details as $detail) {
+                $col1 = new Collection([
+                    [$new->title,$new->created_at, "", "", "", $new->text]
                 ]);
-                if($detail->news_id==$new->id){
+                if ($detail->news_id == $new->id) {
 
                     //صفر را در اکسل نشان نمیداد با این روش این باگ را رفع کردم.
-                    if($detail->effect==0)
-                    $col1=[
-                        [$new->text, $new->title,$detail->important,"0",$detail->comment,$tags[$detail->id]]
-                    ];
+                    if ($detail->effect == 0)
+                        $col1 = [
+                            [$new->title, $new->created_at, $tags[$detail->id], "0", $detail->comment, $new->text]
+                        ];
                     else
-                        $col1=[
-                            [$new->text, $new->title,$detail->important,$detail->effect,$detail->comment,$tags[$detail->id]]
+                        $col1 = [
+                            [$new->title, $new->created_at, $tags[$detail->id], $detail->effect, $detail->comment, $new->text]
                         ];
 
-                    $col2=$col2->toBase()->merge($col1);
-                    $added=true;
+                    $col2 = $col2->toBase()->merge($col1);
+                    $added = true;
                 }
             }
 
-            if($added==false)
-            {
-                $col2=$col2->toBase()->merge($col1);
+            if ($added == false) {
+                $col2 = $col2->toBase()->merge($col1);
             }
-            $added=false;
+            $added = false;
 
         }
         /*$col1=new Collection([
@@ -93,28 +90,26 @@ class NewsExport implements FromCollection, WithHeadings, ShouldAutoSize, WithEv
                     ->setBold(true);
 
 
-                $news=News::all()->reverse();
-                $tags=retriveTags();
-                $details=Details::all();
-                $added=false;
-                $index=1;
-                $colors=array(4 => "00ff1a", 3 => '33ff48', 2 => '66ff76', 1 => 'b3ffbb', 0 => 'ffffff', -1 => 'ff7070', -2 => 'ff4a4a', -3 => 'ff2424', -4 => 'ff0000');
-                foreach ($news as $new)
-                {
-                    foreach($details as $detail)
-                    {
-                        if($detail->news_id==$new->id){
-                                $event->sheet->getDelegate()->getStyle('D'.$index+1)->getFill()
-                                    ->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
-                                    ->getStartColor()
-                                    ->setARGB($colors[$detail->effect]);
-                                $index++;
-                                $added=true;
+                $news = News::all()->reverse();
+                $tags = retriveTags();
+                $details = Details::all();
+                $added = false;
+                $index = 1;
+                $colors = array(4 => "00ff1a", 3 => '33ff48', 2 => '66ff76', 1 => 'b3ffbb', 0 => 'ffffff', -1 => 'ff7070', -2 => 'ff4a4a', -3 => 'ff2424', -4 => 'ff0000');
+                foreach ($news as $new) {
+                    foreach ($details as $detail) {
+                        if ($detail->news_id == $new->id) {
+                            $event->sheet->getDelegate()->getStyle('D' . $index + 1)->getFill()
+                                ->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
+                                ->getStartColor()
+                                ->setARGB($colors[$detail->effect]);
+                            $index++;
+                            $added = true;
                         }
                     }
-                    if($added==false)
+                    if ($added == false)
                         $index++;
-                    $added=false;
+                    $added = false;
                 }
 
                 $event->sheet->getDelegate()->getStyle('A1:F1')->getFill()
