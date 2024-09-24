792738de56e40f87a88dd9ef092142294d1fabe8
mobinaghashahi <mobinaghashahi13@gmail.com>
2024-09-24 19:24:08 +0330

    change export Excel for test

--- app/Exports/NewsExport.php
+++ app/Exports/NewsExport.php
@@ -31,21 +31,21 @@ class NewsExport implements FromCollection, WithHeadings, ShouldAutoSize, WithEv
     {
         $col2 = new Collection([]);
         $news = News::join('details','details.news_id','=','news.id')
-            ->select('news.title as title','news.created_at as created_at','news.text as text',
-                'news.id as id','details.news_id as news_id','details.effect as effect','details.instrument as instrument',
-                'details.comment as comment')
-            ->orderBy('id', 'desc')
+            ->select('news.title as title', 'news.created_at as created_at', 'details.instrument as instrument' , 'details.effect as effect',
+                'details.comment as comment','news.text as text')
+            ->orderBy('news.id', 'desc') // مشخص کردن جدول news برای id
             ->get();
-        foreach ($news as $new) {
-                $col1 = new Collection([
-                    [$new->title,$new->created_at, $new->instrument, "$new->effect", $new->comment, $new->text]
-                ]);
 
-                $col2 = $col2->toBase()->merge($col1);
+        //foreach ($news as $new) {
+        //        $col1 = new Collection([
+        //            [$new->title,$new->created_at, $new->instrument, "$new->effect", $new->comment, $new->text]
+        //        ]);
+//
+        //        $col2 = $col2->toBase()->merge($col1);
+//
+        //}
 
-        }
-
-        return $col2;
+        return $news;
     }
 
     public function registerEvents(): array
@@ -63,21 +63,21 @@ class NewsExport implements FromCollection, WithHeadings, ShouldAutoSize, WithEv
                     ->setBold(true);
 
 
-                $news = News::join('details','details.news_id','=','news.id')
-                    ->select('news.title as title','news.created_at as created_at','news.text as text',
-                        'news.id as id','details.news_id as news_id','details.effect as effect','details.instrument as instrument',
-                        'details.comment as comment')
-                    ->orderBy('id', 'desc')
-                    ->get();
-                $index = 1;
-                $colors = array(4 => "00ff1a", 3 => '33ff48', 2 => '66ff76', 1 => 'b3ffbb', 0 => 'ffffff', -1 => 'ff7070', -2 => 'ff4a4a', -3 => 'ff2424', -4 => 'ff0000');
-                foreach ($news as $new) {
-                            $event->sheet->getDelegate()->getStyle('D' . $index + 1)->getFill()
-                                ->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
-                                ->getStartColor()
-                                ->setARGB($colors[$new->effect]);
-                            $index++;
-                }
+                //$news = News::join('details','details.news_id','=','news.id')
+                //    ->select('news.title as title','news.created_at as created_at','news.text as text',
+                //        'news.id as id','details.news_id as news_id','details.effect as effect','details.instrument as instrument',
+                //        'details.comment as comment')
+                //    ->orderBy('id', 'desc')
+                //    ->get();
+                //$index = 1;
+                //$colors = array(4 => "00ff1a", 3 => '33ff48', 2 => '66ff76', 1 => 'b3ffbb', 0 => 'ffffff', -1 => 'ff7070', -2 => 'ff4a4a', -3 => 'ff2424', -4 => 'ff0000');
+                //foreach ($news as $new) {
+                //            $event->sheet->getDelegate()->getStyle('D' . $index + 1)->getFill()
+                //                ->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
+                //                ->getStartColor()
+                //                ->setARGB($colors[$new->effect]);
+                //            $index++;
+                //}
 
                 $event->sheet->getDelegate()->getStyle('A1:F1')->getFill()
                     ->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
